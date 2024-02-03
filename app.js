5032e3778ce338e42360450e5fc7d49a6596b55f
Kaustav <kaustavpaul12310@gmail.com>
2024-02-03 13:36:18 +0530

    ass3

--- app.js
+++ app.js
@@ -835,6 +835,7 @@ app.use((req, res, next) => {
     //res.status(404).send('<h1>Page not working </h1>');
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
-
+//
 app.listen(3000);
 
+//
